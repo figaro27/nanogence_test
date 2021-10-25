699c1626562d32b7b7b90193e5dfc5efe3847008
figaro27 <sweetfigaro27@gmail.com>
2021-10-25 20:27:39 +0900

    set gap for choose layout

--- src/components/Choose.js
+++ src/components/Choose.js
@@ -3,28 +3,25 @@ import { Link } from "react-router-dom";
 
 const Choose = (props) => {
   return (
-    <div className="grid grid-cols-2">
-
-      <Link to="/cement">
-        <div className="choose_item">
+    <div className="grid gap-6 grid-cols-2">
+      <div className="choose_item">
+        <Link to="/cement">
           <img src="images/cement_logo.jpg" className="px-16 py-4" alt="cement" />
           <div className="choose_item_text">
             <span className="m-2 md:m-4 text-2xl">Cement</span>
             <span className="text-lg">(Clinker producer, distributor, retailor)</span>
           </div>
-        </div>
-      </Link>
-
-      <Link to="/concrete">
-        <div className="choose_item">
+        </Link>
+      </div>
+      <div className="choose_item">
+        <Link to="/concrete">
           <img src="images/concrete_logo.jpg" className="px-16 py-4" alt="cement" />
           <div className="choose_item_text">
             <span className="m-2 md:m-4 text-2xl">Concrete</span>
             <span className="text-lg">(RMX, Precast, Drycast, mortar )</span>
           </div>
-        </div>
-      </Link>
-
+        </Link>
+      </div>
     </div>
   )
 }
